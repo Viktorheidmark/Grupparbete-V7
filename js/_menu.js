457e98f71f02e15bc3dc0b6142f578c5199ba1e9
Tannaz Yadollahi <tannaz.yadollahi@learnet.se>
2025-04-16 13:54:41 +0200

    done

--- js/_menu.js
+++ js/_menu.js
@@ -4,6 +4,5 @@ createMenu('Statistics Template JS', [
   { name: 'Jämför två år', script: 'compare-two-years.js' },
   { name: 'Hitta trender', script: 'trends.js' },
   { name: 'test', script: 'test.js' },
-  { name: 'mongoDB', script: 'kommun-query.mongodb' },
   { name: 'mongoDB', script: 'kommun-query.mongodb' }
 ]);
