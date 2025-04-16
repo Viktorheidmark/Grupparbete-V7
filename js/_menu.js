04528d65b0a59d65cb229537fcbd6d0991ebc205
Viktorheidmark <viktorheidmark1@gmail.com>
2025-04-16 12:24:07 +0200

    Initial commit

---
+++ js/_menu.js
@@ -0,0 +1,8 @@
+createMenu('Statistics Template JS', [
+  { name: 'Nytt i version 7', script: 'new-in-v7.js' },
+  { name: 'Visa ett år', script: 'one-year.js' },
+  { name: 'Jämför två år', script: 'compare-two-years.js' },
+  { name: 'Hitta trender', script: 'trends.js' },
+  { name: 'test', script: 'test.js' },
+  { name: 'mongoDB', script: 'kommun-query.mongodb'}
+]);
