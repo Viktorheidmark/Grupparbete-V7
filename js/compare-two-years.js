ed56784ef5cecb52faff3cd835ab22aec78aa476
Benereta Hox <beneretahoxha8@gmail.com>
2025-04-16 23:04:41 +0200

    uppdatering Ben

--- js/compare-two-years.js
+++ js/compare-two-years.js
@@ -1,9 +1,9 @@
-
+// With addMdToPage("intro") you can add a title and text to the page. The text is in markdown format, so you can use all the features of markdown. You 
 addMdToPage("Intro")
 
+// This is a simple example of how to use the dbQuery function to get data from the database and display it in a table and a chart.
 let county = await dbQuery("SELECT lan, folkmangd2024 FROM countyinfo")
 tableFromData({ data: county })
-
 
 
 // Draw a Google Charts
