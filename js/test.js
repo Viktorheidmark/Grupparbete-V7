6618c1768c59967298fdb75cc779b9162f1d9929
Viktorheidmark <viktorheidmark1@gmail.com>
2025-04-16 15:53:56 +0200

    commit

--- js/test.js
+++ js/test.js
@@ -1,1 +1,5 @@
-let data = await dbQUERY
+dbQuery.use('kommun-info-mongodb');
+let income = await dbQuery.collection('incomeByKommun').find({}).limit(25);
+console.log('income from mongodb', income);
+
+tableFromData({ data: income })
