c7b33803892b0f2d03ddf08217a28746170ef2f2
Viktorheidmark <viktorheidmark1@gmail.com>
2025-04-17 13:01:31 +0200

    commit

--- js/_menu.js
+++ js/_menu.js
@@ -1,9 +1,4 @@
 createMenu('Statistics Template JS', [
   { name: 'Nytt i version 7', script: 'new-in-v7.js' },
-  { name: 'Visa ett år', script: 'one-year.js' },
-  { name: 'Histogram', script: 'compare-two-years.js' },
-  { name: 'Hitta trender', script: 'trends.js' },
-  { name: 'test', script: 'test.js' },
-  { name: 'mongoDB', script: 'kommun-query.mongodb' },
-  { name: "Jämförelse av två år", script: "jamforelse-av-tva-ar.js" },
+  { name: 'Visa ett år', script: 'intro.js' }
 ]);
--- js/test.js
+++
@@ -1,5 +0,0 @@
-dbQuery.use('kommun-info-mongodb');
-let income = await dbQuery.collection('incomeByKommun').find({}).limit(25);
-console.log('income from mongodb', income);
-
-tableFromData({ data: income })
