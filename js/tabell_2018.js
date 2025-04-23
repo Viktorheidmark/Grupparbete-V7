33af0a319ea675971230ed3540fbc0e1517df113
Viktorheidmark <viktorheidmark1@gmail.com>
2025-04-23 12:57:40 +0200

    commit

--- js/tabell_2018.js
+++ js/tabell_2018.js
@@ -1,5 +1,10 @@
+addMdToPage("# Här är en lista över databasen undersokning_2018 i SQLite ")
+
+
 dbQuery.use('undersokning_2018');
 
 let under = await dbQuery('SELECT * FROM roster_2018')
 
 tableFromData({ data: under })
+
+addMdToPage("Källa: https://www.val.se/valresultat/riksdag-region-och-kommun/2018/valresultat.html")
--- js/tabell_2022.js
+++ js/tabell_2022.js
@@ -1,5 +1,10 @@
+addMdToPage("# Här är en lista över databasen undersokning_2018 i SQLite ")
+
+
 dbQuery.use('undersokning_2022');
 
 let under = await dbQuery('SELECT * FROM roster_2022')
 
 tableFromData({ data: under })
+
+addMdToPage("Källa: https://resultat.val.se/val2022/RD?r=S")
