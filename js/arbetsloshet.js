d5d636edf567404b890e981bcb2680ae1abc5118
Viktorheidmark <viktorheidmark1@gmail.com>
2025-04-25 13:37:25 +0200

    commit

--- js/arbetsloshet.js
+++ js/arbetsloshet.js
@@ -1,3 +1,7 @@
 let result = await dbQuery("SELECT * FROM inget_jobb LIMIT 10");
 
-tableFromData({ data: result });
+if (result && result.length > 0) {
+  tableFromData({ data: result });
+} else {
+  addMdToPage("⚠️ Tabellen `inget_jobb` finns, men innehåller inga rader.");
+}
