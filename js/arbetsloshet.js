4a088f5b5a7f87a21ff74e0d8ac38d4496efd454
Viktorheidmark <viktorheidmark1@gmail.com>
2025-04-29 12:51:42 +0200

    commit

--- js/arbetsloshet.js
+++ js/arbetsloshet.js
@@ -1,5 +1,7 @@
 dbQuery.use("arbetsloshet")
 
-let county = await dbQuery("SELECT * FROM arbetsloshet")
+let county = await dbQuery("SELECT * FROM arbetsloshet LIMIT 10")
 
 tableFromData({ data: county })
+
+addMdToPage("Källa: https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/START__AM__AM0210__AM0210D/ArRegArbStatus/sortedtable/tableViewSorted/")
