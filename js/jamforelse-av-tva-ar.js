39fec04c17a08e79d97a112eb0ed1edd245557c9
Benereta Hox <beneretahoxha8@gmail.com>
2025-04-30 09:07:25 +0200

    updater

--- js/jamforelse-av-tva-ar.js
+++ js/jamforelse-av-tva-ar.js
@@ -10,14 +10,14 @@ const högarbetslöshetCommunes = ['Flen', 'Perstorp', 'Malmö', 'Eskilstuna', '
 const lågarbetslöshetCommunes = ['Pajala', 'Kiruna', 'Kungsbacka', 'Tjörn', 'Öckerö', 'Krokom', 'Sotenäs', 'Gällivare', 'Habo', 'Mörbylånga'];
 
 =======
- 
+
 // Hämta valresultat
 let electionResultsForWork = await dbQuery('MATCH (n:Partiresultat) RETURN n');
- 
+
 // Lista med kommuner baserat på arbetslöshetsnivå
 const högarbetslöshetCommunes = ['Flen', 'Perstorp', 'Malmö', 'Eskilstuna', 'Fagersta', 'Sandviken', 'Ronneby', 'Filipstad', 'Södertälje', 'Söderhamn'];
 const lågarbetslöshetCommunes = ['Pajala', 'Kiruna', 'Kungsbacka', 'Tjörn', 'Öckerö', 'Krokom', 'Sotenäs', 'Gällivare', 'Habo', 'Mörbylånga'];
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 // Filtrera valresultat för hög och låg arbetslöshet separat
 let filteredHog = electionResultsForWork.filter(({ kommun }) =>
@@ -29,7 +29,7 @@ let filteredLag = electionResultsForWork.filter(({ kommun }) =>
 <<<<<<< HEAD
 
 =======
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 // Funktion för att skapa sammanställning
 function skapaSammanstallning(filteredData) {
@@ -42,7 +42,7 @@ function skapaSammanstallning(filteredData) {
 <<<<<<< HEAD
 
 =======
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
   return Object.entries(grupperad).map(([kommun, list]) => {
     let vinnare2018 = list.reduce((max, curr) => curr.roster2018 > max.roster2018 ? curr : max);
@@ -51,7 +51,7 @@ function skapaSammanstallning(filteredData) {
 <<<<<<< HEAD
 
 =======
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
     return {
       kommun,
@@ -74,14 +74,14 @@ let sammanstallningLag = skapaSammanstallning(filteredLag);
 addMdToPage("### Kommuner med hög arbetslöshet");
 
 =======
- 
+
 // Skapa sammanställningar
 let sammanstallningHog = skapaSammanstallning(filteredHog);
 let sammanstallningLag = skapaSammanstallning(filteredLag);
- 
+
 // Tabell för kommuner med hög arbetslöshet
 addMdToPage("### Kommuner med hög arbetslöshet");
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 sammanstallningHog = [...sammanstallningHog].sort((a, b) => {
   if (a.byte === "Ja" && b.byte !== "Ja") return -1;
@@ -91,7 +91,7 @@ sammanstallningHog = [...sammanstallningHog].sort((a, b) => {
 <<<<<<< HEAD
 
 =======
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 tableFromData({
   data: sammanstallningHog.map(row => ({
@@ -110,10 +110,10 @@ tableFromData({
 addMdToPage("### Kommuner med låg arbetslöshet");
 
 =======
- 
+
 // Tabell för kommuner med låg arbetslöshet
 addMdToPage("### Kommuner med låg arbetslöshet");
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 sammanstallningLag = [...sammanstallningLag].sort((a, b) => {
   if (a.byte === "Ja" && b.byte !== "Ja") return -1;
@@ -123,7 +123,7 @@ sammanstallningLag = [...sammanstallningLag].sort((a, b) => {
 <<<<<<< HEAD
 
 =======
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 tableFromData({
   data: sammanstallningLag.map(row => ({
@@ -149,17 +149,17 @@ let combinedFiltered = [...filteredHog, ...filteredLag];
 let sdData = combinedFiltered.filter(({ parti }) => parti === 'Sverigedemokraterna');
 
 =======
- 
- 
+
+
 // Tabell för Sverigedemokraternas röster i de valda kommunerna
 addMdToPage("### Sverigedemokraternas röster i valda kommuner");
- 
+
 // Kombinera filtrerade data (hög + låg arbetslöshet)
 let combinedFiltered = [...filteredHog, ...filteredLag];
- 
+
 // Filtrera ut endast Sverigedemokraterna
 let sdData = combinedFiltered.filter(({ parti }) => parti === 'Sverigedemokraterna');
- 
+
 >>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 // Grupp per kommun
 let sdSammanstallning = {};
@@ -172,30 +172,16 @@ for (let item of sdData) {
     differens: (roster2022 || 0) - (roster2018 || 0)
   };
 }
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 // Skapa tabell
 tableFromData({
   data: Object.values(sdSammanstallning)
 });
-<<<<<<< HEAD
 
 
 
 
 
 
-=======
- 
- 
- 
- 
- 
- 
->>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 // Skapa fullständig sammanställning för alla kommuner i Sverige
 function skapaFullSammanstallning(data) {
   let grupperad = {};
@@ -204,20 +190,10 @@ function skapaFullSammanstallning(data) {
     if (!grupperad[kommun]) grupperad[kommun] = [];
     grupperad[kommun].push({ parti, roster2018, roster2022 });
   }
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
   return Object.entries(grupperad).map(([kommun, list]) => {
     let vinnare2018 = list.reduce((max, curr) => curr.roster2018 > max.roster2018 ? curr : max);
     let vinnare2022 = list.reduce((max, curr) => curr.roster2022 > max.roster2022 ? curr : max);
     const byttParti = vinnare2018.parti !== vinnare2022.parti;
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
     return {
       kommun,
       vinnare2018: vinnare2018.parti,
@@ -229,28 +205,16 @@ function skapaFullSammanstallning(data) {
     };
   });
 }
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 // Skapa och sortera sammanställningen för hela landet
 let sammanstallningAllaKommuner = skapaFullSammanstallning(electionResultsForWork).sort((a, b) => {
   if (a.byte === "Ja" && b.byte !== "Ja") return -1;
   if (a.byte !== "Ja" && b.byte === "Ja") return 1;
   return 0;
 });
-<<<<<<< HEAD
 
 // Tabell för alla kommuner i Sverige
 addMdToPage("### Alla kommuner i Sverige – sorterade efter partibyte");
 
-=======
- 
-// Tabell för alla kommuner i Sverige
-addMdToPage("### Alla kommuner i Sverige – sorterade efter partibyte");
- 
->>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
 tableFromData({
   data: sammanstallningAllaKommuner.map(row => ({
     kommun: row.kommun,
@@ -261,8 +225,4 @@ tableFromData({
     byte: row.byte,
     differens: row.differens
   }))
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 9bbfc454238669ac6f6cd28ed0afd457eb8a1737
